d643abe103ea0a6073a67077f653fcd0ca20298e
Yan Musikhin <yanmusikhin@gmail.com>
2022-04-08 14:28:37 +0400

    add burger menu

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,22 +1,30 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import { useAppSelector } from 'hooks/useAppSelector';
-import { Nav, MenuList, MenuItem, ItemLink } from 'components/menu/style';
+import { Nav, MenuList, MenuItem, ItemLink, Blur, Burger, BurgetIcon } from 'components/menu/style';
 
 export const Menu = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const {categories} = useAppSelector(state => state.product)
   return (
-    <Nav>
-      <MenuList>
-        {
-          categories?.map((menuItem: any) => (
-            <MenuItem key={menuItem}>
-              <Link href={`/category/${menuItem}`} passHref>
-                <ItemLink>{menuItem}</ItemLink>
-              </Link>
-            </MenuItem>
-          ))
-        }
-      </MenuList>
+    <Nav onClick={() => setIsOpen(false)}>
+      <Blur isOpen={isOpen}/>
+      <div onClick={(e) => e.stopPropagation()}>
+        <Burger onClick={() => setIsOpen(prevState => !prevState)} isOpen={isOpen}>
+          <BurgetIcon isOpen={isOpen}/>
+        </Burger>
+        <MenuList isOpen={isOpen}>
+          {
+            categories?.map((menuItem: string) => (
+              <MenuItem key={menuItem}>
+                <Link href={`/category/${menuItem}`} passHref>
+                  <ItemLink onClick={() => setIsOpen(false)}>{menuItem}</ItemLink>
+                </Link>
+              </MenuItem>
+            ))
+          }
+        </MenuList>
+      </div>
     </Nav>
   );
 };
--- src/components/menu/style.ts
+++ src/components/menu/style.ts
@@ -1,18 +1,32 @@
 import styled from 'styled-components';
 import { BREAKPOINTS } from 'contants/breakpoints';
 import { COLORS } from 'contants/colors';
-import { FONT_SIZES } from 'contants/font';
+import { FONT_SIZES, FONT_WEIGHT } from 'contants/font';
 
 export const Nav = styled.nav`
 `;
 
-export const MenuList = styled.ul`
+export const MenuList = styled.ul<{isOpen?: boolean}>`
   display: flex;
   column-gap: 32px;
-  @media (max-width: ${BREAKPOINTS.sm}) {
+  @media (max-width: ${BREAKPOINTS.md}) {
+    display: flex;
     flex-direction: column;
-    align-items: center;
-    gap: 5px;
+    justify-content: center;
+    gap: 15px;
+    position: fixed;
+    visibility: ${props => !props.isOpen ? 'hidden' : 'visibility'};
+    z-index: 2;
+    top: 0;
+    left: ${props => !props.isOpen ? '-100%' : 0};
+    width: 80%;
+    height: 100%;
+    margin: 0;
+    padding: 0 20px;
+    list-style: none;
+    background-color: ${COLORS.BURGER_BG};
+    box-shadow: 1px 0 6px ${COLORS.MIDDLE_GREY};
+    transition-duration: .25s;
   }
 `;
 
@@ -38,4 +52,66 @@ export const ItemLink = styled.a`
   &:hover::after {
     display: block;
   }
+  @media (max-width: ${BREAKPOINTS.md}){
+    font-weight: ${FONT_WEIGHT.xl};
+    justify-content: center;
+  }
 `;
+
+export const Blur = styled.div<{isOpen:boolean}>`
+  ${props => props.isOpen && 
+          `@media (max-width: ${BREAKPOINTS.md}) {
+    backdrop-filter: blur(2px);
+    position: fixed;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 1;
+  `}
+`;
+export const Burger = styled.div<{isOpen: boolean}>`
+  @media (max-width: ${BREAKPOINTS.md}) {
+    z-index: 3;
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    position: ${props => !props.isOpen ? `relative` : `fixed`};
+    top: ${props => props.isOpen && '76px'};
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      width: ${props => props.isOpen ? `29px` : `20px`};
+      height: 2px;
+      background-color: #000;
+      transform: ${props => props.isOpen && `rotate(-45deg) translateY(50%)`};
+      transform-origin: bottom left;
+    }
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      width: ${props => props.isOpen ? `29px` : `20px`};
+      height: 2px;
+      background-color: #000;
+      transform: ${props => props.isOpen && `rotate(45deg) translateY(-50%)`};
+      transform-origin: top left;
+
+    }
+  }
+`;
+export const BurgetIcon = styled.span<{isOpen: boolean}>`
+  @media (max-width: ${BREAKPOINTS.md}) {
+    ${props => props.isOpen && `
+    transform: scale(0.1);
+  `}
+    position: absolute;
+    bottom: 9px;
+    width: 20px;
+    height: 2px;
+    background-color: #000;
+  }
+  `;
