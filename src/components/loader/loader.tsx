ed3a48ff2f8d1c98c0d91c4e933a12a877860538
Yan Musikhin <yanmusikhin@gmail.com>
2022-04-25 11:33:14 +0400

    fix: edit interface

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { AnimationBox, Animation } from 'components/loader/style';
 
 interface LoaderProps {
-  top: string,
+  top?: string,
 }
 
 export const Loader:FC<LoaderProps> = ({top = '50%'}) => (
