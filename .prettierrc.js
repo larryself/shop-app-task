45ecad75d4ed0fe8a18b03b3b8d4dbecc5143d8d
Yan Musikhin <yanmusikhin@gmail.com>
2022-06-08 16:20:10 +0400

    feat: add eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "all",
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  semi: true,
+  arrowParens: 'avoid',
+  endOfLine: 'auto'
+}
