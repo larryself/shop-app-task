74a5d0eccd49514a7a077ef49b457baa0995b0b8
Yan Musikhin <yanmusikhin@gmail.com>
2022-06-08 16:18:18 +0400

    fix: del redux store

--- src/hooks/useProductSelector.ts
+++
@@ -1,9 +0,0 @@
-import { useAppSelector } from 'hooks/useAppSelector';
-import { createSelector } from '@reduxjs/toolkit';
-import { TypeRootState } from 'store/store';
-
-const selector = createSelector((state: TypeRootState) => state.product, (product) => product)
-export const useProductSelector = () => {
-  const product = useAppSelector(selector);
-  return product
-}
